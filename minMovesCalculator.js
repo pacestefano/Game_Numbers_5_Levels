2cc02824fc92eef5c2b2a733375fa6708f472d9a
pacestefano <135709394+pacestefano@users.noreply.github.com>
2024-07-05 18:47:55 +0200

    Update minMovesCalculator.js

--- minMovesCalculator.js
+++ minMovesCalculator.js
@@ -1,5 +1,5 @@
 export function calculateMinMoves(puzzle) {
-    const goal = [1, 2, 3, 4, 5, 6, 7, 8, 0]; // Stato finale obiettivo
+    const goal = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // Stato finale obiettivo
     const visited = new Set();
     let minMoves = Infinity;
 
